2d9d43239e78348b401dbf6f2fc0f572a9484ae1
Kevin Reynolds <kevintreynolds30@gmail.com>
2019-12-24 13:10:28 -0700

    day1 part1 & part2

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode.Days;
+using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalFuel = 0;
+
+            var day1 = new Day1();
+
+            foreach (var mass in day1.input) {
+                totalFuel += day1.CalculatFuelForMass(mass);
+            }
+
+            Console.WriteLine(totalFuel);
+        }
+    }
+}
